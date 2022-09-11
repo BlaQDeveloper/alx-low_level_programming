68c6a2e1d0f086da44337b70efda69cf11e6cf37
BlaQDeveloper <ikechukwudavid331@gmail.com>
2022-09-11 15:55:59 -0700

    Using the printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  *
  * Description: Using the main function
  *this program prints "with proper grammar, but the outcome is a piece of art"
- *Return : Always 0
+ *Return: Always 0
  */
 int main(void)
 {
