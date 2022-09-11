dc5a63027880ff4292282494274eed0b4e319f4c
BlaQDeveloper <ikechukwudavid331@gmail.com>
2022-09-11 15:15:47 -0700

    4. Hello , puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+/**
+ * main: scripts that uses the function puts
+ *
+ * Description: using of the main function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
