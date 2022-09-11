0018f9014821c8c7e1456e0073a626e96bf98822
BlaQDeveloper <ikechukwudavid331@gmail.com>
2022-09-11 15:28:03 -0700

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main: scripts that uses the function puts
  *
  * Description: using of the main function
- *
+ *this program prints "Programming is like building a multilingual puzzle"
  * Return: Always 0
  */
 int main(void)
