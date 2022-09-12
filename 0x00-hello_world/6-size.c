8722573d03631bcce429ca48e5ff3d691bfb0080
BlaQDeveloper <ikechukwudavid331@gmail.com>
2022-09-12 05:34:38 -0700

    Size of various data types of a computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(a));
-	printf("Size of an int: %ld byte(s)\n", sizeof(b));
-	printf("Size of long int: %ld byte(s)\n", sizeof(c));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(d));
-	printf("Size of float: %ld byte(s)\n", sizeof(e));
+	printf(stderr, "Size of a char: %ld byte(s)\n", sizeof(a));
+	printf(stderr, "Size of an int: %ld byte(s)\n", sizeof(b));
+	printf(stderr, "Size of long int: %ld byte(s)\n", sizeof(c));
+	printf(stderr, "Size of long long int: %ld byte(s)\n", sizeof(d));
+	printf(stderr, "Size of float: %ld byte(s)\n", sizeof(e));
 	return (0);
 }
