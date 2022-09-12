96b8b14fff8919ace64fd239294f5b6939dcef60
BlaQDeveloper <ikechukwudavid331@gmail.com>
2022-09-12 06:01:57 -0700

    Stressed out

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -8,6 +7,8 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	char str1[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
 	return (1);
 }
