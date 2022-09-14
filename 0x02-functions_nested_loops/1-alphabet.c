25dcc6de2715d4f5b9061610e73b2bbb382f6f7b
BlaQDeveloper <ikechukwudavid331@gmail.com>
2022-09-14 06:16:35 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - function to print abc
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alhabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
