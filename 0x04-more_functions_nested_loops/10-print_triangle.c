f7cf3437ce1038c688c008720de967864cb03238
BlaQDeveloper <ikechukwudavid331@gmail.com>
2022-09-16 17:35:54 -0700

    quick fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,17 +15,11 @@ void print_triangle(int size)
 		for ((inc1 = 1; inc1 <= size; inc1++))
 		{
 			for ((inc2 = size - 1); inc2 > 0; inc2--)
-			{
 				putchar(' ');
-			}
 			for ((inc2 = 0; inc2 < inc1; inc2++))
-			{
 				putchar('#');
-			}
 			if (inc1 == size)
-			{
 				continue;
-			}
 			putchar('\n');
 		}
 	}
