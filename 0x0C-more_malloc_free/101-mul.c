89b88f68cd0f8cf2612663c20a330cff7166504b
BlaQDeveloper <ikechukwudavid331@gmail.com>
2022-10-07 08:58:41 -0700

    Quickfix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -90,8 +90,7 @@ void init(char *str, int l)
 
 	for (i = 0; i < l; i++)
 		str[i] = '0';
-	str[i] = '\0'
-		a;
+	str[i] = '\0';
 }
 /**
  * main - multiply two numbers
@@ -124,7 +123,7 @@ int main(int argc, char *argv[])
 			_putchar(e[ti]);
 		exit(98);
 	}
-	init(a, ln - 1);
+	init(a; ln - 1);
 	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 	{
 		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
