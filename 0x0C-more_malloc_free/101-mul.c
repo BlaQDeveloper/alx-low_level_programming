61e061b16339769479efb5fab897085c139cee74
BlaQDeveloper <ikechukwudavid331@gmail.com>
2022-10-06 20:00:55 -0700

    Advanced task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -71,11 +71,8 @@ int check_for_digits(char **av)
 	{
 		for (j = 0; av[i][j]; j++)
 		{
-			if (av[i][j]; j++)
-			{
-				if (av[i][j] < '0' || av[i][j] > '9')
-					return (1);
-			}
+			if (av[i][j] < '0' || av[i][j] > '9')
+				return (1);
 		}
 	}
 	return (0);
