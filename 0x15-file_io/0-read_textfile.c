bd3904198cd173595ac3cf84d5a171be537409d0
BlaQDeveloper <ikechukwudavid331@gmail.com>
2022-11-03 21:44:59 -0700

    hotfix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file, n_read, wrote;
-	char *bufffer;
+	char *buffer;
 
 	buffer = malloc(sizeof(*buffer) * (letters + 1));
 	if (filename == NULL || buffer == NULL)
