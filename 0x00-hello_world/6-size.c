7b6956ff3fd81c0ef389a54d4e79182af0de11a7
BlaQDeveloper <ikechukwudavid331@gmail.com>
2023-12-16 12:22:04 -0800

    Doing hard things

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long int d;
 	double e;
 
-	printf("Size of a int: %lu byte(s)\n", sizeof(int);
-	printf("Size of a char: %lu byte(s)\n", sizeof(char);
-	printf("Size of a float: %lu byte(s)\n", sizeof(float);
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int);
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int);
-	printf("Size of a double: %lu byte(s)\n", sizeof(double);
+	printf("Size of a int: %lu byte(s)\n", sizeof(i);
+	printf("Size of a char: %lu byte(s)\n", sizeof(a);
+	printf("Size of a float: %lu byte(s)\n", sizeof(b);
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c);
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d);
+	printf("Size of a double: %lu byte(s)\n", sizeof(e);
 	return (0);
 }
