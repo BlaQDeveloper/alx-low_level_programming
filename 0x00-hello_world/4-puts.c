377dbef98bce4fbb431d350354529d690999c093
BlaQDeveloper <ikechukwudavid331@gmail.com>
2023-12-16 10:10:06 -0800

    Betty stop disturbing me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /*
- * main - prints a string using the put function.
- * 
+ * main - print the string in the put function
  * Description: using main function
  * this program prints Programming is like building a multilingual puzzle.
  * Return: 0
