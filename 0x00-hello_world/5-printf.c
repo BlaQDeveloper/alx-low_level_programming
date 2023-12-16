2f69668b5afcf1df7babafba923f4ec8008c1bf3
BlaQDeveloper <ikechukwudavid331@gmail.com>
2023-12-16 11:22:58 -0800

    GIT PUSH

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - print the string using printf function
  * Description: using the main function
  * prints with proper grammar, but the outcome is a piece of art,
@@ -9,4 +9,5 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }
