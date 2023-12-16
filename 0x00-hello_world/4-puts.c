ff8368628f9f9a6f4eb8289c9cb7b9c014b2c6f6
BlaQDeveloper <ikechukwudavid331@gmail.com>
2023-12-16 09:05:46 -0800

    Betty no fuck me up

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/*
+ * main: prints a sentence which is a string
+ * Description: using the puts function
+ * this program prints Programing is like building a multilingual puzzle, 
+ * folowed by a new line.
+ * Return: 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts('"Programming is like building a multilingual puzzle\n');
 	return (0);
 }
