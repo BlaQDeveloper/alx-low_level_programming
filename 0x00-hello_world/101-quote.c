3602581a0c552f824f982e672e133b16126b53c1
BlaQDeveloper <ikechukwudavid331@gmail.com>
2023-12-16 21:33:46 -0800

    Trying to know why my code is not working

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - ENtry point
+ * Description: priint out a string without using printf function
+ * Return: 1
+ */
+int main(void)
+{
+	write(
