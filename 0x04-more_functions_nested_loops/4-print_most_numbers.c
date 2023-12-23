a73dedd252b895a372d4dcc0fea8e704ebc833dc
BlaQDeveloper <ikechukwudavid331@gmail.com>
2023-12-22 20:27:05 -0800

    Wdf is wrong me

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
 *print_most_numbers - prints numbers except 2 and 4
 *
 *Return: returns nothing
 */
-
 void print_most_numbers(void)
 {
-	int n;
-
-	for (n = 48; n < 58; n++)
-	{
-		if ((n == 50) || (n == 52))
-		{
-			continue;
-		}
-		prutchar(n);
-	}
-	putchar(10);
+int number;
+for (number = 48; number < 58; number++)
+{
+if ((number == 50) || (number == 52))
+{
+continue;
+}
+_putchar(number);
+}
+_putchar(10);
 }
