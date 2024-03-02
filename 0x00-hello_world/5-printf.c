c2056faff7e6da0f27089d90c4d8c7d4f5a0d033
BlaQDeveloper <ikechukwudavid331@gmail.com>
2024-03-02 17:55:34 +0100

    Task done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print out a string with printf function
+ * Description: using the main function
+ * prints a string using printf function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
