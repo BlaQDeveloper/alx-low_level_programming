89b7b61edc7048cda90f0b3ebb97f0fafc9eb324
BlaQDeveloper <ikechukwudavid331@gmail.com>
2024-03-02 19:31:49 +0100

    Task Done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - using fput function to print string
+ * 
+ * Description: using the main function
+ * prints and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * Return: 1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
