7cf8ef049301948715b25ef64300bf6869139bf7
BlaQDeveloper <ikechukwudavid331@gmail.com>
2024-03-22 23:56:04 +0100

    Task done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet
+ * Description: using the main function 
+ * print the alphabet in lowercase
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
