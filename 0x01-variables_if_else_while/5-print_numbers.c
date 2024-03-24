c565258f52e8e6b790d30a6d91869f38143867c0
BlaQDeveloper <ikechukwudavid331@gmail.com>
2024-03-24 04:11:25 +0100

    Task done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print out numbers from 0 to 9
+ * Description: using the main function
+ * print out numbers from 0 to 9
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
+	
+	for (numbers = 0; numbers <= 9; ch++)
+	{
+		printf("%d\n", numbers);
+	}
+	return (0);
+}
